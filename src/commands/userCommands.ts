0b8d610bb412e108265f9b906993eee0807d59e5
Bratah123 <brandonnguyen3301@gmail.com>
2021-02-06 00:14:44 -0800

    add info command

--- src/commands/userCommands.ts
+++ src/commands/userCommands.ts
@@ -1,15 +1,39 @@
 import { 
     Discord,
     Command,
-    CommandMessage
+    CommandMessage,
+    Description
  } from "@typeit/discord";
 
+ import { MessageEmbed } from "discord.js";
+ import config = require("../config.json")
 
-@Discord("!")
+@Discord(config.commandPrefix)
+@Description("All User commands handlers.")
 abstract class UserCommands {
 
-    @Command("hello")
-    private hello(message: CommandMessage) {
-        message.channel.send("Hello from TypeScript!");
+    @Command("info")
+    private handleInfo(message: CommandMessage) {
+        const serverInfo = {
+            'Exp': '1x',
+            'Item': '1x',
+            'Meso': '1x',
+            'Server State': 'N/A',
+            'Server Location': 'N/A',
+            'Release Date': 'N/A',
+        }
+        const messageEmbed = new MessageEmbed()
+        .setColor(0x0000ff)
+        .setTitle(config.serverName)
+        .setDescription("KMS v316")
+        .setThumbnail(config.serverImg);
+
+        for (let key in serverInfo) {
+            let value = serverInfo[key];
+            messageEmbed.addField(key, value, true);
+        }
+        messageEmbed.setFooter(config.serverName)
+
+        message.channel.send(messageEmbed);
     }
 }
